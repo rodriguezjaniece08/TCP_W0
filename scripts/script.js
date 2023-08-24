ac9ae004ddbb01d5f820cf8d238ed5e7a138fc82
Janice R <rodriguezjaniece@gmail.com>
2023-08-24 12:36:20 -0400

    added reset functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,13 @@
-// JQUERY
+// Jquery
 
-$(document).ready(function() {
+$(document).ready(function(){
     let borderStyles = ['solid', 'dotted', 'dashed', 'double'];
 
-    $('.change-button').on('click', function() {
+    $('.change-button').on('click', function(){
         var boxNumber = $(this).data('box-number');
         if (boxNumber) {
             $('.box[data-box-number="' + boxNumber + '"]').css({
-                'background-color': randomColor(),
+                'background-color': randomColor(), 
                 'border-radius': Math.floor(Math.random() * 25) + 'px',
                 'border-style': borderStyles[Math.floor(Math.random() * borderStyles.length)],
                 'border-color': randomColor(),
@@ -16,10 +16,11 @@ $(document).ready(function() {
         }
     });
 
-    $('#change').on('click', function() {
+
+    $('#change').on('click', function(){
         $('.box').each(function() {
             $(this).css({
-                'background-color': randomColor(),
+                'background-color': randomColor(), 
                 'border-radius': Math.floor(Math.random() * 25) + 'px',
                 'border-style': borderStyles[Math.floor(Math.random() * borderStyles.length)],
                 'border-color': randomColor(),
@@ -28,23 +29,41 @@ $(document).ready(function() {
         });
     });
 
-    $('.hide-button').on('click', function() {
-        var boxNumber = $(this).index() + 1; // Assuming the index corresponds to the box number
-        $('.box[data-box-number="' + boxNumber + '"]').css('visibility', 'hidden');
-    });
-
-    $('.hide-all').on('click', function() {
-        $('.box').css('visibility', 'hidden');
-    });
-
-    $('.show-all').on('click', function() {
-        $('.box').css('visibility', 'visible');
-    });
-
     function randomColor() {
-        return 'rgb(' + Math.round(Math.random() * 255) + ',' + Math.round(Math.random() * 255) + ',' + Math.round(Math.random() * 255) + ')';
+        return 'rgb(' + Math.round(Math.random()*255) + ',' + Math.round(Math.random()*255) + ',' + Math.round(Math.random()*255) + ')';
     }
+
+    $('.hide-button').on('click', function(){
+        var boxNumber = $(this).data('box-number');
+        if (boxNumber !== undefined) {
+            $('.box[data-box-number="' + boxNumber + '"]').css('visibility', 'hidden');
+        } 
+    });
+
+    $('#hide').on('click', function(){
+        $('.hide-all').css('visibility', 'hidden');
+    });
+
+    $('#show').on('click', function(){
+        $('.show-all').css('visibility', 'visible');
+    });
+
+    $('#reset').on('click', function(){
+        $('.first').css({
+            'background-color': '#f9665e',
+            'border': '4px solid black'
+        })
+        $('.sec').css({
+            'background-color': '#799fcb',
+            'border': '5px dashed yellow'
+        })
+        $('.third').css({
+            'background-color': '#A9c3B6',
+            'border': '4px solid magenta'
+        })
+    })
 });
+
 
 
 
